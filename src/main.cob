5bc0fb06fcdfac010c31a14f5bc8b74ed73d1e60
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-11 15:05:52 +0200

    style: Indent all COBOL statements in main.cob

--- src/main.cob
+++ src/main.cob
@@ -1,46 +1,47 @@
 >>SOURCE FORMAT IS FREE
 IDENTIFICATION DIVISION.
-PROGRAM-ID. Sorting-Files.
+  PROGRAM-ID. Sorting-Files.
 
 ENVIRONMENT DIVISION.
-INPUT-OUTPUT SECTION.
-FILE-CONTROL.
-  *> The file to sort
-  SELECT Input-File ASSIGN TO "Input.txt"
-  ORGANISATION LINE SEQUENTIAL
-  ACCESS SEQUENTIAL.
+  INPUT-OUTPUT SECTION.
+    FILE-CONTROL.
+      *> The file to sort
+      SELECT Input-File ASSIGN TO "Input.txt"
+      ORGANISATION LINE SEQUENTIAL
+      ACCESS SEQUENTIAL.
 
-  *> Sort work file
-  SELECT Sort-File *> name provided internally by COBOL
-  ORGANISATION LINE SEQUENTIAL
-  ACCESS SEQUENTIAL.
+      *> Sort work file (internal to SORT statement)
+      SELECT Sort-File
+      ORGANISATION LINE SEQUENTIAL
+      ACCESS SEQUENTIAL.
 
-  *> Sort output file
-  SELECT Out-File ASSIGN TO "Output.txt"
-  ORGANISATION LINE SEQUENTIAL
-  ACCESS SEQUENTIAL.
+      *> Sort output file
+      SELECT Out-File ASSIGN TO "Output.txt"
+      ORGANISATION LINE SEQUENTIAL
+      ACCESS SEQUENTIAL.
 
 DATA DIVISION.
-FILE DIVISION.
-  FD Input-File.
-  01 Input-Rec.
-    05 In-UID PIC 9(4).
-    05 In-Username PIC X(25).
-    05 In-User-Age PIC 999.
+  FILE DIVISION.
+    FD Input-File.
+      01 Input-Rec.
+        05 In-UID PIC 9(4).
+        05 In-Username PIC X(25).
+        05 In-User-Age PIC 999.
 
-  SD Sort-File.
-  01 Sort-Rec.
-    05 Sf-UID PIC 9(4).
-    05 Sf-Username PIC X(25).
-    05 Sf-User-Age PIC 999.
+    SD Sort-File.
+      01 Sort-Rec.
+        05 Sf-UID PIC 9(4).
+        05 Sf-Username PIC X(25).
+        05 Sf-User-Age PIC 999.
 
-  FD Out-File.
-  01 Out-Rec.
-    05 Out-UID PIC 9(4).
-    05 Out-Username PIC X(25).
-    05 Out-User-Age PIC 999.
+    FD Out-File.
+      01 Out-Rec.
+        05 Out-UID PIC 9(4).
+        05 Out-Username PIC X(25).
+        05 Out-User-Age PIC 999.
 
 PROCEDURE DIVISION.
   DISPLAY "Hello, World!".
   STOP RUN.
+
 END PROGRAM Sorting-Files.
