679f14621f2fe32cf6dd5580853fce011a82ea78
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-12 05:31:20 +0200

    feat(file): Add comma separators to file defs

--- src/main.cob
+++ src/main.cob
@@ -25,19 +25,25 @@ DATA DIVISION.
     FD Input-File.
       01 Input-Rec.
         05 In-UID PIC 9(4).
+        05 FILLER PIC X VALUE ",".
         05 In-Username PIC X(25).
+        05 FILLER PIC X VALUE ",".
         05 In-User-Age PIC 999.
 
     SD Sort-File.
       01 Sort-Rec.
         05 Sf-UID PIC 9(4).
+        05 FILLER PIC X VALUE ",".
         05 Sf-Username PIC X(25).
+        05 FILLER PIC X VALUE ",".
         05 Sf-User-Age PIC 999.
 
     FD Out-File.
       01 Out-Rec.
         05 Out-UID PIC 9(4).
+        05 FILLER PIC X VALUE ",".
         05 Out-Username PIC X(25).
+        05 FILLER PIC X VALUE ",".
         05 Out-User-Age PIC 999.
 
 PROCEDURE DIVISION.
