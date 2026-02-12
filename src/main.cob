6bcd4855a816614ce0dbe602483d5ca2ddbcca17
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-12 06:05:58 +0200

    feat: Increase size of the files' username fields

--- src/main.cob
+++ src/main.cob
@@ -26,7 +26,7 @@ DATA DIVISION.
       01 Input-Rec.
         05 In-UID PIC 9(4).
         05 FILLER PIC X VALUE ",".
-        05 In-Username PIC X(25).
+        05 In-Username PIC X(45).
         05 FILLER PIC X VALUE ",".
         05 In-User-Age PIC 999.
 
@@ -34,7 +34,7 @@ DATA DIVISION.
       01 Sort-Rec.
         05 Sf-UID PIC 9(4).
         05 FILLER PIC X VALUE ",".
-        05 Sf-Username PIC X(25).
+        05 Sf-Username PIC X(45).
         05 FILLER PIC X VALUE ",".
         05 Sf-User-Age PIC 999.
 
@@ -42,7 +42,7 @@ DATA DIVISION.
       01 Out-Rec.
         05 Out-UID PIC 9(4).
         05 FILLER PIC X VALUE ",".
-        05 Out-Username PIC X(25).
+        05 Out-Username PIC X(45).
         05 FILLER PIC X VALUE ",".
         05 Out-User-Age PIC 999.
 
